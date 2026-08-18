58b89127d463a1e16b8ef0b0c90ebb9562303a38
Mahmoud <mahmoudhaskir01@gmail.com>
2026-08-18 15:45:19 +0200

    BereichDto hinzugefügt

---
+++ LernPlattform.Shared/Dtos/BereichDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LernPlattform.Shared.Dtos
+{
+    public  class BereichDto
+    {
+        public int Id { get; set; }
+        public string Kuerzel { get; set; } = string.Empty;
+        public string Titel { get; set; } = string.Empty;
+        public string? Beschreibung { get; set; }
+        public string Farbe { get; set; } = "#2E5395";
+    }
+}
