369453e70084bdc2bf05b1cf31a16dbd3875c5b2
Mahmoud <mahmoudhaskir01@gmail.com>
2026-09-05 18:54:20 +0200

    KurseController mit GET /api/kurse und KursDto ergaenzt

---
+++ LernPlattform.Api/Controllers/KursController.cs
@@ -0,0 +1,30 @@
+﻿using LernPlattform.Api.Data;
+using LernPlattform.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace LernPlattform.Api.Controllers
+{
+    [ApiController]
+    [Route("api/kurse")]
+    public class KursController:ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public KursController(AppDbContext db)
+        {
+            _db= db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<KursDto>>> GetKurse()
+        {
+            var Kurse = await _db.Kurse.OrderBy(b => b.Reihenfolge).Select(b => new KursDto
+            {
+                Id = b.Id,
+                Bezeichnung = b.Bezeichnung,
+                Farbe = b.Farbe
+            }).ToListAsync();
+
+            return Ok(Kurse);
+            
+        }
+    }
+}
---
+++ LernPlattform.Shared/Dtos/KursDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LernPlattform.Shared.Dtos
+{
+    public class KursDto
+    {
+        public int Id { get; set; }
+        public string Bezeichnung { get; set; } = "";
+        public string Farbe { get; set; } = "";
+    }
+}
